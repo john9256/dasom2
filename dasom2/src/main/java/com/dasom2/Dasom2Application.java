a279b797ac0e5c0aeb9d54f44cffda8f5fd39d97
ksw <john9256@naver.com>
2024-08-29 00:54:26 +0900

    aws 배포를 위한 application 파일 수정

--- dasom2/src/main/java/com/dasom2/Dasom2Application.java
+++ dasom2/src/main/java/com/dasom2/Dasom2Application.java
@@ -2,12 +2,18 @@ package com.dasom2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Dasom2Application {
+public class Dasom2Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Dasom2Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Dasom2Application.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Dasom2Application.class, args);
+    }
 }
